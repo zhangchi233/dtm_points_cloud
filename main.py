67b600632e5d183a45c8eeec8b1113d80ef9b03c
Chi zhang <60132725+zhangchi233@users.noreply.github.com>
2023-01-07 21:45:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import laspy
-
 import os
 path = r"F:\dem1015\assignment4"
 os.chdir(path)
-file = "C_58EZ1.laz"
-las=laspy.read(file)
-# it may cost some times
-midx,midy = 0.5*(las.header.x_min + las.header.x_max),0.5*las.header.y_max+0.5*las.header.y_max
-bound_box =[midx+250,midy+250,midx-250,midy-250]
+print("start to read")
 
-# crop the file chunck by chunck
-header = las.header
 
-with laspy.open(file) as f:
-    with laspy.open("somepath.las", mode="w", header=header) as writer:
-        for points in f.chunk_iterator(1_000_000):
-            x = points.x.copy()
-            y = points.y.copy()
-            mask = (x <= bound_box[0]) & (x >= bound_box[2]) & (y >= bound_box[3] ) & (y <= bound_box[1])
-            if True in mask:
-                sub_points=points[mask]
-                writer.write_points(sub_points)
-                print(mask)
+# use pickle to fast read the points information
+import pickle
+#with open("database.txt",'wb') as f:
+    #pickle.dump(points, f, fix_imports=True, buffer_callback=None)
+
+with open("database.txt",'rb') as f:
+    particles = pickle.load(f)
+print("has read read points")
+import points_grid
+from points_grid import cloth
+
+points_grid.draw_3d_points(particles, '^')
+clothes = cloth(particles)
+
+particle_xyz = clothes.implementation_CSF(0.03)
+points_grid.draw_3d_points(particle_xyz, '^')
+# remeber to generate database.txt
