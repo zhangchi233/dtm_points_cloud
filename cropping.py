318e5977afef6f4f2b69529d252adb40dd500242
max_zhang <1155124276@link.cuhk.edu.hk>
2023-01-19 18:15:01 +0100

    modify extend

--- cropping.py
+++ cropping.py
@@ -10,13 +10,13 @@
 import laspy
 
 import os
-'''
+
 path = r"F:\dem1015\assignment4"
 os.chdir(path)
 file = "C_58EZ1.laz"
 las=laspy.read(file)
 # it may cost some times
-midx,midy = 0.5*(las.header.x_min + las.header.x_max),0.5*las.header.y_max+0.5*las.header.y_max
+midx,midy = 0.5*(las.header.x_min + las.header.x_max),0.5*las.header.y_min+0.5*las.header.y_max
 bound_box =[midx+250,midy+250,midx-250,midy-250]
 
 # crop the file chunck by chunck
@@ -32,7 +32,7 @@ with laspy.open(file) as f:
                 sub_points=points[mask]
                 writer.write_points(sub_points)
                 print(mask)
-'''
+
 
 path = r"F:\dem1015\assignment4"
 os.chdir(path)
@@ -44,7 +44,7 @@ points = list(zip(list(las.x),list(las.y),list(las.z)))
 
 # use pickle to fast read the points information
 import pickle
-#with open("database.txt",'wb') as f:
-    #pickle.dump(points, f, fix_imports=True, buffer_callback=None)
+with open("database.txt",'wb') as f:
+    pickle.dump(points, f, fix_imports=True, buffer_callback=None)
 
 #use pickle is much faster then directly read laz files
